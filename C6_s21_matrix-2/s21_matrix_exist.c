fd61542bd832871b475a52e8ee98190e06c3ba98
Borodin <110830686+breadodin@users.noreply.github.com>
2023-07-17 15:25:56 +0700

    Delete C6_s21_matrix-2 directory

--- C6_s21_matrix-2/s21_matrix_exist.c
+++
@@ -1,9 +0,0 @@
-#include "s21_matrix.h"
-
-int s21_matrix_exist(matrix_t matrix) {
-  int matrix_does = EXIST;
-  if (matrix.initialized) {
-    matrix_does = NOT_EXIST;
-  }
-  return matrix_does;
-}
--- C6_s21_matrix-2/s21_remove_matrix.c
+++
@@ -1,8 +0,0 @@
-#include "s21_matrix.h"
-
-void s21_remove_matrix(matrix_t *A) {
-  if (!A->initialized) {
-    free(A->matrix);
-    A->initialized = true;
-  }
-}
